e8f70935d3654506a1b2dd44eb7598d444f812a4
Canyon Schwartz <canyon@pop-os.localdomain>
2019-07-25 20:00:23 -0700

    Finished logging comments section to console

--- comments.js
+++ comments.js
@@ -1,41 +1,41 @@
-(function (window) {
+(function(window) {
+'use strict';
+
+fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      let articleArea = document.getElementById('articlestart');
+      json.forEach(function(post) {
+        articleArea.innerHTML += `<article><h2>Post ${post['id']}. ${
+            post['title']}</h2><p>${post['body']}</p></article>`;
+      });
+    });
+
+const BUTTON_SELECTOR = '[data-posts="id"]';
+
+let buttons = document.querySelectorAll(BUTTON_SELECTOR);
+
+buttons.forEach(function(button) {
   'use strict';
 
-  fetch('http://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(json => 
-    {
-      console.log(json);
-      let body = document.querySelector("body");
-      json.forEach(function(post){
-        body.innerHTML += `<article><h2>Post ${post["id"]}. ${post["title"]}</h2><p>${post["body"]}</p></article>`;
-      });
-      
-    });
-    
+  let sectionSelector = `#comments-${button.value}`;
+  let commentSection = document.querySelector(sectionSelector);
 
-  
+  button.addEventListener('click', function(event) {
+    if (commentSection.hidden) {
+      commentSection.hidden = false;
+      button.textContent = 'Hide comments';
+    } else {
+      commentSection.hidden = true;
+      button.textContent = 'Show comments';
+    }
 
-  const BUTTON_SELECTOR = '[data-posts="id"]';
+    fetch(`http://jsonplaceholder.typicode.com/comments?postId=${button.value}`)
+        .then(response => response.json())
+        .then(json => console.log(json));
 
-  let buttons = document.querySelectorAll(BUTTON_SELECTOR);
-
-  buttons.forEach(function (button) {
-    'use strict';
-
-    let sectionSelector = `#comments-${button.value}`;
-    let commentSection = document.querySelector(sectionSelector);
-
-    button.addEventListener('click', function (event) {
-      if (commentSection.hidden) {
-        commentSection.hidden = false;
-        button.textContent = 'Hide comments';
-      } else {
-        commentSection.hidden = true;
-        button.textContent = 'Show comments';
-      }
-
-      event.preventDefault();
-    });
+    event.preventDefault();
   });
+});
 })(window);
